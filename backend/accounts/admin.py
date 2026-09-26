b41f5ca677f30972d23a77adb2fc3d0f0537e3f0
MD Rafiqual Islam <rafiqual69@gmail.com>
2026-09-26 13:08:22 +0600

    Harden WorkerProfile admin financial state

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -5,12 +5,30 @@ from .models import WorkerProfile
 
 @admin.register(WorkerProfile)
 class WorkerProfileAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        # Worker profiles are created by the account/application flow.
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        # Profiles contain financial state and must not be deleted from admin.
+        return False
+
     list_display = (
         "user",
         "balance",
+        "reserved_balance",
         "total_earned",
         "completed_tasks",
         "created_at",
     )
     search_fields = ("user__username", "user__email")
     list_filter = ("created_at",)
+
+    readonly_fields = (
+        "user",
+        "balance",
+        "reserved_balance",
+        "total_earned",
+        "completed_tasks",
+        "created_at",
+    )
