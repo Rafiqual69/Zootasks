6f8c53adf9cd5a7ad30ebee487efc0423fb6ea28
MD Rafiqual Islam <rafiqual69@gmail.com>
2026-09-26 14:44:46 +0600

    Expose advertiser promotion request route

--- backend/promotions/urls.py
+++ backend/promotions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import marketplace, start_promotion, submit_promotion
+from .views import advertiser_create_promotion, marketplace, start_promotion, submit_promotion
 
 urlpatterns = [
     path("", marketplace, name="promotion_marketplace"),
+    path("advertiser/create/", advertiser_create_promotion, name="advertiser_create_promotion"),
     path("<int:promotion_id>/start/", start_promotion, name="start_promotion"),
     path("<int:promotion_id>/submit/", submit_promotion, name="submit_promotion"),
 ]
