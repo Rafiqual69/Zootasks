ad3795a043ce5f7c43b2e6224ae58f3c04b553d5
MD Rafiqual Islam <rafiqual69@gmail.com>
2026-09-26 17:25:38 +0600

    Merge promotion funding and option migration branches

---
+++ backend/promotions/migrations/0006_merge_promotion_funding_and_options.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("promotions", "0003_promotionfunding_ledger"),
+        ("promotions", "0005_alter_promotion_options"),
+    ]
+
+    operations = []
